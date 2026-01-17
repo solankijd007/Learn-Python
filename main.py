9039c12d58afa52a55bc935806e523fdcf403141
Jagdish Solanki <solankijd5182@gmail.com>
2026-01-17 15:07:59 +0530

    [Add] :- CRUD

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI 
 from src.utils.db import Base, engine
+from src.tasks.touter import task_routes
 
 Base.metadata.create_all(engine)
 
 app = FastAPI(title="This is my task management application.")
+app.include_router(task_routes)
